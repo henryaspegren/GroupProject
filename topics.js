14cffc575396594a3329d6a750a90c6a02ecf09b
Joel <Joel>
2016-02-07 10:39:38 +0000

    First demo of frontend - topic selection; messages; one-way infinite scroll, using dummy data

---
+++ topics.js
@@ -0,0 +1,129 @@
+var loadTopics, newTopic;
+
+d3.select("#input-topic").on("keydown", function() {
+  var e;
+  e = d3.event;
+  if (e.which === 13 || e.which === 10) {
+    e.preventDefault();
+    return newTopic(d3.select(this).text());
+  }
+}).on("click", function() {
+  return d3.select(this).text("");
+});
+
+newTopic = function(topic) {
+  var response;
+  console.log("<Things happen to " + topic + ">");
+  response = {
+    length: 10,
+    top_topics: [
+      {
+        name: "Zamorak",
+        topic_id: 1,
+        number: 3
+      }, {
+        name: "Saradomin",
+        topic_id: 2,
+        number: 3
+      }, {
+        name: "Guthix",
+        topic_id: 3,
+        number: 3
+      }, {
+        name: "Armadyl",
+        topic_id: 4,
+        number: 3
+      }, {
+        name: "Seren",
+        topic_id: 5,
+        number: 3
+      }, {
+        name: "Bandos",
+        topic_id: 6,
+        number: 3
+      }, {
+        name: "Icthlarin",
+        topic_id: 8,
+        number: 3
+      }, {
+        name: "Amascut",
+        topic_id: 9,
+        number: 3
+      }, {
+        name: "Elidinis",
+        topic_id: 10,
+        number: 3
+      }, {
+        name: "Scabaras",
+        topic_id: 11,
+        number: 3
+      }, {
+        name: "Tumeken",
+        topic_id: 12,
+        number: 3
+      }, {
+        name: "Crondis",
+        topic_id: 13,
+        number: 3
+      }, {
+        name: "Zaros",
+        topic_id: 14,
+        number: 3
+      }, {
+        name: "Marimbo",
+        topic_id: 15,
+        number: 3
+      }, {
+        name: "Jas",
+        topic_id: 16,
+        number: 3
+      }, {
+        name: "Bik",
+        topic_id: 17,
+        number: 3
+      }, {
+        name: "Mah",
+        topic_id: 18,
+        number: 3
+      }, {
+        name: "Tuska",
+        topic_id: 19,
+        number: 3
+      }, {
+        name: "Brassica Prime",
+        topic_id: 20,
+        number: 3
+      }
+    ]
+  };
+  print(topic);
+  loadTopics([]);
+  return loadTopics(response.top_topics);
+};
+
+loadTopics = function(topics) {
+  var btns, data;
+  data = d3.select("#topics").selectAll("li").data(topics, function(topic) {
+    return topic.name;
+  });
+  btns = data.enter().append("li").style("opacity", 0).text(function(topic) {
+    return topic.name;
+  });
+  btns.transition().delay(function(topic, i) {
+    return i * 100;
+  }).style("opacity", 1);
+  btns.on("mouseover", function() {
+    return d3.select(this).transition().style("font-size", "32px");
+  }).on("mouseout", function() {
+    return d3.select(this).transition().style("font-size", "24px");
+  }).on("click", function(topic) {
+    d3.select("#input-topic").text(topic.name);
+    return newTopic(topic.name);
+  });
+  data.style("opacity", 0).transition().delay(function(topic, i) {
+    return i * 100;
+  }).style("opacity", 1).text(function(topic) {
+    return topic.name;
+  });
+  return data.exit().transition().style("opacity", 0).remove();
+};
