3c594adad97c818a5131c23edee1c9c22310c66f
Joel <Joel>
2016-02-10 17:54:52 +0000

    Amazing Disappearing Buttons were driving me insane, still no luck -> using :hover CSS instead Added request to server to populate topic list. Will default to dummy topics if unavailable. But stilll using dummy chat messages. Increased animation speed

--- topics.coffee
+++ topics.coffee
@@ -1,3 +1,24 @@
+testResponse = {
+  length: 10,
+  top_topics: [
+    {name: "Zamorak",   topic_id:  1, number: 3,},
+    {name: "Saradomin", topic_id:  2, number: 3,},
+    {name: "Guthix",    topic_id:  3, number: 3,},
+    {name: "Armadyl",   topic_id:  4, number: 3,},
+    {name: "Seren",     topic_id:  5, number: 3,},
+    {name: "Bandos",    topic_id:  6, number: 3,},
+    {name: "Icthlarin", topic_id:  8, number: 3,},
+    {name: "Amascut",   topic_id:  9, number: 3,},
+    {name: "Elidinis",  topic_id: 10, number: 3,},
+    {name: "Scabaras",  topic_id: 11, number: 3,},
+    {name: "Zaros",     topic_id: 12, number: 3,},
+    {name: "Marimbo",   topic_id: 13, number: 3,},
+    {name: "Jas",       topic_id: 14, number: 3,},
+    {name: "Bik",       topic_id: 15, number: 3,},
+    {name: "Mah",       topic_id: 16, number: 3,},
+    {name: "Brassica Prime", topic_id: 17, number: 3,},
+  ],
+}
 
 d3.select("#input-topic").on "keydown", ->
   e = d3.event
@@ -8,65 +29,40 @@ d3.select("#input-topic").on "keydown", ->
 
 newTopic = (topic) ->
   console.log("<Things happen to #{topic}>")
-  response = {
-    length: 10,
-    top_topics: [
-      {name: "Zamorak",   topic_id:  1, number: 3,},
-      {name: "Saradomin", topic_id:  2, number: 3,},
-      {name: "Guthix",    topic_id:  3, number: 3,},
-      {name: "Armadyl",   topic_id:  4, number: 3,},
-      {name: "Seren",     topic_id:  5, number: 3,},
-      {name: "Bandos",    topic_id:  6, number: 3,},
-      {name: "Icthlarin", topic_id:  8, number: 3,},
-      {name: "Amascut",   topic_id:  9, number: 3,},
-      {name: "Elidinis",  topic_id: 10, number: 3,},
-      {name: "Scabaras",  topic_id: 11, number: 3,},
-      {name: "Tumeken",   topic_id: 12, number: 3,},
-      {name: "Crondis",   topic_id: 13, number: 3,},
-      {name: "Zaros",     topic_id: 14, number: 3,},
-      {name: "Marimbo",   topic_id: 15, number: 3,},
-      {name: "Jas",       topic_id: 16, number: 3,},
-      {name: "Bik",       topic_id: 17, number: 3,},
-      {name: "Mah",       topic_id: 18, number: 3,},
-      {name: "Tuska",     topic_id: 19, number: 3,},
-      {name: "Brassica Prime", topic_id: 20, number: 3,},
-    ],
-  }
-  loadTopics []
-  loadTopics response.top_topics
+
+  request = JSON.stringify { search_phrase: topic, limit: 10 }
+
+  d3.json "http://localhost:5000/top_topics_by_search_phrase/"
+    .header "Content-Type", "application/json"
+    .post request, (error, json) ->
+      if error?
+        console.log error
+        response = testResponse
+      else
+        response = json
+
+      print topic
+      loadTopics d3.shuffle response.top_topics
 
 loadTopics = (topics) ->
   data = d3.select "#topics"
            .selectAll "li"
-           .data topics, (topic) -> topic.name
+           .data topics
 
   btns = data.enter()
       .append "li"
-      .style "opacity", 0
-      .text (topic) -> topic.name
-
-  btns.transition()
-      .delay (topic, i) -> i * 100
-      .style "opacity", 1
-
-  btns.on "mouseover", ->
-        d3.select this
-          .transition()
-          .style "font-size", "32px"
-      .on "mouseout", ->
-        d3.select this
-          .transition()
-          .style "font-size", "24px"
-      .on "click", (topic) ->
-        d3.select "#input-topic"
-          .text topic.name
-          newTopic topic.name
 
   data.style "opacity", 0
-      .transition()
-      .delay (topic, i) -> i * 100
-      .style "opacity", 1
       .text (topic) -> topic.name
+      .transition()
+      .delay (topic, i) -> i * 50
+      .style "opacity", 1
+      .each "end", ->
+        d3.select this
+          .on "click", (topic) ->
+              d3.select "#input-topic"
+                .text topic.name
+              newTopic topic.name
 
   data.exit()
       .transition()
--- topics.js
+++ topics.js
@@ -1,4 +1,75 @@
-var loadTopics, newTopic;
+var loadTopics, newTopic, testResponse;
+
+testResponse = {
+  length: 10,
+  top_topics: [
+    {
+      name: "Zamorak",
+      topic_id: 1,
+      number: 3
+    }, {
+      name: "Saradomin",
+      topic_id: 2,
+      number: 3
+    }, {
+      name: "Guthix",
+      topic_id: 3,
+      number: 3
+    }, {
+      name: "Armadyl",
+      topic_id: 4,
+      number: 3
+    }, {
+      name: "Seren",
+      topic_id: 5,
+      number: 3
+    }, {
+      name: "Bandos",
+      topic_id: 6,
+      number: 3
+    }, {
+      name: "Icthlarin",
+      topic_id: 8,
+      number: 3
+    }, {
+      name: "Amascut",
+      topic_id: 9,
+      number: 3
+    }, {
+      name: "Elidinis",
+      topic_id: 10,
+      number: 3
+    }, {
+      name: "Scabaras",
+      topic_id: 11,
+      number: 3
+    }, {
+      name: "Zaros",
+      topic_id: 12,
+      number: 3
+    }, {
+      name: "Marimbo",
+      topic_id: 13,
+      number: 3
+    }, {
+      name: "Jas",
+      topic_id: 14,
+      number: 3
+    }, {
+      name: "Bik",
+      topic_id: 15,
+      number: 3
+    }, {
+      name: "Mah",
+      topic_id: 16,
+      number: 3
+    }, {
+      name: "Brassica Prime",
+      topic_id: 17,
+      number: 3
+    }
+  ]
+};
 
 d3.select("#input-topic").on("keydown", function() {
   var e;
@@ -12,118 +83,38 @@ d3.select("#input-topic").on("keydown", function() {
 });
 
 newTopic = function(topic) {
-  var response;
+  var request;
   console.log("<Things happen to " + topic + ">");
-  response = {
-    length: 10,
-    top_topics: [
-      {
-        name: "Zamorak",
-        topic_id: 1,
-        number: 3
-      }, {
-        name: "Saradomin",
-        topic_id: 2,
-        number: 3
-      }, {
-        name: "Guthix",
-        topic_id: 3,
-        number: 3
-      }, {
-        name: "Armadyl",
-        topic_id: 4,
-        number: 3
-      }, {
-        name: "Seren",
-        topic_id: 5,
-        number: 3
-      }, {
-        name: "Bandos",
-        topic_id: 6,
-        number: 3
-      }, {
-        name: "Icthlarin",
-        topic_id: 8,
-        number: 3
-      }, {
-        name: "Amascut",
-        topic_id: 9,
-        number: 3
-      }, {
-        name: "Elidinis",
-        topic_id: 10,
-        number: 3
-      }, {
-        name: "Scabaras",
-        topic_id: 11,
-        number: 3
-      }, {
-        name: "Tumeken",
-        topic_id: 12,
-        number: 3
-      }, {
-        name: "Crondis",
-        topic_id: 13,
-        number: 3
-      }, {
-        name: "Zaros",
-        topic_id: 14,
-        number: 3
-      }, {
-        name: "Marimbo",
-        topic_id: 15,
-        number: 3
-      }, {
-        name: "Jas",
-        topic_id: 16,
-        number: 3
-      }, {
-        name: "Bik",
-        topic_id: 17,
-        number: 3
-      }, {
-        name: "Mah",
-        topic_id: 18,
-        number: 3
-      }, {
-        name: "Tuska",
-        topic_id: 19,
-        number: 3
-      }, {
-        name: "Brassica Prime",
-        topic_id: 20,
-        number: 3
-      }
-    ]
-  };
-  print(topic);
-  loadTopics([]);
-  return loadTopics(response.top_topics);
+  request = JSON.stringify({
+    search_phrase: topic,
+    limit: 10
+  });
+  return d3.json("http://localhost:5000/top_topics_by_search_phrase/").header("Content-Type", "application/json").post(request, function(error, json) {
+    var response;
+    if (error != null) {
+      console.log(error);
+      response = testResponse;
+    } else {
+      response = json;
+    }
+    print(topic);
+    return loadTopics(d3.shuffle(response.top_topics));
+  });
 };
 
 loadTopics = function(topics) {
   var btns, data;
-  data = d3.select("#topics").selectAll("li").data(topics, function(topic) {
+  data = d3.select("#topics").selectAll("li").data(topics);
+  btns = data.enter().append("li");
+  data.style("opacity", 0).text(function(topic) {
     return topic.name;
-  });
-  btns = data.enter().append("li").style("opacity", 0).text(function(topic) {
-    return topic.name;
-  });
-  btns.transition().delay(function(topic, i) {
-    return i * 100;
-  }).style("opacity", 1);
-  btns.on("mouseover", function() {
-    return d3.select(this).transition().style("font-size", "32px");
-  }).on("mouseout", function() {
-    return d3.select(this).transition().style("font-size", "24px");
-  }).on("click", function(topic) {
-    d3.select("#input-topic").text(topic.name);
-    return newTopic(topic.name);
-  });
-  data.style("opacity", 0).transition().delay(function(topic, i) {
-    return i * 100;
-  }).style("opacity", 1).text(function(topic) {
-    return topic.name;
+  }).transition().delay(function(topic, i) {
+    return i * 50;
+  }).style("opacity", 1).each("end", function() {
+    return d3.select(this).on("click", function(topic) {
+      d3.select("#input-topic").text(topic.name);
+      return newTopic(topic.name);
+    });
   });
   return data.exit().transition().style("opacity", 0).remove();
 };
