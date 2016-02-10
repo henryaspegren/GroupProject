212529702250c25534a1c191a47ad90a3e470f99
vk313 <vk313@cam.ac.uk>
2016-02-10 21:41:39 +0000

    changed to post requests

--- messages_panel.js
+++ messages_panel.js
@@ -8,43 +8,49 @@ $(document).ready(function () {
 		$(".scroll-pos").text(scroll);
 		$(".scroll-height").text(height);
 		if (isScrolledToEnd) {
-			//var moreMessages = request(topic, 50, linkToAPI); // Get 50 more messages ---- Not active because not yet connected to API
-
-			var moreMessages = {number:3 , messages: [   {ID: "m1", user_ID: "user1", post:"Hello"}, // won't need this later
-													   {ID: "m2", user_ID: "user2", post:"Hello2"} ,
-													   {ID: "m3", user_ID: "user3", post:"Hello3"}] };
-			for(var i=0;i<moreMessages.messages.length;i++){
-				$("#left").append(moreMessages.messages[i].user_ID + ": " + moreMessages.messages[i].post + "<br></br>"); // Append user:message
-			}
+			var json = new Object();
+			json.topic = topic;
+			json.limit = 50;
+			request(linkToAPI,json,callback);
 		}
 	});
 });
 
 
-//var request = function(topic, limit,linkToAPI){}		//This is the JSON request
-//	$.ajax({
-//		url: linkToAPI,
-//		data: {"topic":topic, "limit":limit},
-//		type: "GET",
-//		success: function (data) {
-//			var obj = JSON.parse(data);
-//			return obj;
-//		}
-//	});
-//};
+
+
+var request = function (url, json, callback) {
+	$.ajax({
+         url: url,
+         type: "POST",
+         data: json,
+         dataType: "json",
+         contentType: "application/json",
+         success: callback(data)
+});
+
+var callback = function(data){
+	var obj = JSON.parse(data);
+	for(var i=0; i<obj.messages.length;i++){
+		var currentLine = obj.messages[i].user_ID + ": " + obj.messages[i].post;
+		$("#left").append(currentLine+"<br> </br>");
+	}
+	
+}
+
 
 var print = function (topic) {
-		// var messagesArray = request(topic,50,linkToAPI )
-		var messagesArray = {
-			number: 3,
-			messages: [
-				{ID: "m1", user_ID: "DragonSlayer_z0rg_0456", post: "I can't say how much I love "+topic},  // won't need this either
-				{ID: "m2", user_ID: "x0x0xx_pk_x0xx0x", post: "JAGEX "+topic+" IS SO OP PLZ REMOVE THX"} ,
-				{ID: "m3", user_ID: "BranstonPickle82", post: topic+" boss guide: 1. Go to "+topic+"'s lair. 2. Kill "+topic+". 3. ????? 4. Profit!"},
-			]
-		};
-		for(var i=0; i<messagesArray.messages.length;i++){
-			var currentLine = messagesArray.messages[i].user_ID + ": " + messagesArray.messages[i].post;
-			$("#left").append(currentLine+"<br> </br>");
-		}
+	var json = new Object();
+	json.topic = topic;
+	json.limit = 50;
+	request(linkToAPI,json,callback);
+	//var messagesArray = {
+	//	number: 3,
+	//	messages: [
+	//		{ID: "m1", user_ID: "DragonSlayer_z0rg_0456", post: "I can't say how much I love "+topic},  // won't need this either
+	//		{ID: "m2", user_ID: "x0x0xx_pk_x0xx0x", post: "JAGEX "+topic+" IS SO OP PLZ REMOVE THX"} ,
+	//		{ID: "m3", user_ID: "BranstonPickle82", post: topic+" boss guide: 1. Go to "+topic+"'s lair. 2. Kill "+topic+". 3. ????? 4. Profit!"},
+	//	]
+	//};
+	
 };
